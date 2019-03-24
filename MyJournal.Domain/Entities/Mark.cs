f76331292297b17114fb5ac37b99b928eb5bc4da
Vladyslav Biletskyi <vladyslav.biletskyi@nure.ua>
2019-03-24 22:55:14 +0200

    Mark instance updated

--- MyJournal.Domain/Entities/Mark.cs
+++ MyJournal.Domain/Entities/Mark.cs
@@ -1,9 +1,13 @@
-﻿namespace MyJournal.Domain.Entities
+﻿using System;
+
+namespace MyJournal.Domain.Entities
 {
     public class Mark : BaseInstance
     {
         public Attend Attend { get; set; }
 
         public int Grade { get; set; }
+
+        public DateTime UpdateTime { get; set; }
     }
 }
